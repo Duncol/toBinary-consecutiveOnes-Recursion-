35be8d9972bdefeb27126f92d692d6b1d2427820
Duncol <damek90@gmail.com>
2016-10-10 11:36:26 +0200

    Update solution.java

--- solution.java
+++ solution.java
@@ -2,14 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class MainClass {
-    static String binStr = "";
-    static int i = 0;
-    static int n = 0;
-    static int maxCount = 0;
+    static String binStr = "";              //Base for building binary equivalent od given decimal number in toBinary()
+    
+                                            //Variables used in consecutiveOnes()
+    static int i = 0;                       // count used to acces specific index of binStr
+    static int n = 0;                       // current count of consecutive 1's, n++ if 1st 1 is encountered; resets when 0 is found
+    static int maxCount = 0;                // used to store maximum amount of cosecutive 1's in case when incidental 0 resets n, which would be higher than following 1's after that 0, i.e. 11101111
+    static boolean isFirstDive = true;      // key to return maxValue instead of n, but still make recursion based on n (makes method to pass specified condition only once)
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int sourceNumber = scan.nextInt();
-        System.out.println(consecutiveOnes(i, n, toBinary(sourceNumber)));
+        System.out.println(consecutiveOnes(i, n, toBinary(sourceNumber), isFirstDive)); //Main "thing"
     }
     public static String toBinary(int n){
         if (n==1) {return binStr += "1";}
@@ -24,7 +28,7 @@ public class MainClass {
             }
         }
     }
-    public static int consecutiveOnes(int i, int n, String s){
+    public static int consecutiveOnes(int i, int n, String s, boolean iFD){
         if (i == s.length()-1){
             if(s.charAt(i) == '1'){
                 return 1;
@@ -33,15 +37,19 @@ public class MainClass {
                 return 0;
             }
         }
+        else if (iFD){                                  //First call of method - always
+            iFD = false;
+            n = consecutiveOnes(i, n, s, iFD);
+            return maxCount = Math.max(n, maxCount);
+        }
         else{
             if (s.charAt(i) == '1'){
-                n = consecutiveOnes(i+1, n, s);         //<<< 1st         <<<2nd
+                n = consecutiveOnes(i+1, n, s, iFD);
                 return n + 1;
             }
             else{
-                n = consecutiveOnes(i+1, n, s);         //<<< 3rd
+                n = consecutiveOnes(i+1, n, s, iFD);
                 maxCount = Math.max(n, maxCount);
-                n = 0;
                 return 0;
             }
         }
